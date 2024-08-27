3d6fcc0f37388135b0bd45454e0f540d19a29f85
Franco Ortega <francopdx@gmail.com>
2024-08-27 11:56:22 -0700

    fix font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-	return (
-		<section className='py-24'>
-			<div className='container'>
-				<h1 className='text-3x1 font-bold'>Portfolio</h1>
-			</div>
-		</section>
-	);
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Portfolio</h1>
+      </div>
+    </section>
+  );
 }
