89d775394e181c1f1bdd20dfb5923b9b3dcba538
Franco Ortega <francopdx@gmail.com>
2024-08-27 17:27:05 -0700

    add social link urls

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Portfolio</h1>
       </div>
     </section>
-  );
+  )
 }
