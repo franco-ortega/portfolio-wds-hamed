15ff44f1182228f362b279cb0079addc1ea2399f
Franco Ortega <francopdx@gmail.com>
2024-08-28 15:09:38 -0700

    add title and prose global classes

--- components/ui/intro.tsx
+++ components/ui/intro.tsx
@@ -6,31 +6,29 @@ export default function Intro({}: Props) {
   return (
     <section className={styles.section}>
       <div className={styles.flex}>
-        <h1 className={styles.title}>
-          <p className={styles.content}>
-            Detail-oriented software engineer with a passion for creating
-            user-focused products. Thrives in collaborative, cross-functional,
-            agile environments. Adept at developing innovative features,
-            improving existing systems, creating efficient internal tools, and
-            debugging issues within business applications. Approaches work
-            optimistically to think big while balancing practicality to
-            consistently achieve goals.
-          </p>
-          <p className={styles.content}>
-            Additionally, despite growing up very much a math and science kid,
-            has learned to love and nurture creativity through writing,
-            cartography, and board game design. Other sources of joy are
-            bouldering, bike rides, art shows, and visiting friends who have
-            landed in faraway places. Finally, roller hockey, punk rock, and
-            zines were big influences in the past that provided a lot of
-            learnings around team work, expressiveness, and how the world could
-            be a more equitable place.
-          </p>
-          <p className={styles.content}>
-            Yes to racial justice, social justice, climate justice, and economic
-            justice.
-          </p>
-        </h1>
+        <h1 className={styles.title}>Hey, I'm Franco</h1>
+        <p className={styles.content}>
+          Detail-oriented software engineer with a passion for creating
+          user-focused products. Thrives in collaborative, cross-functional,
+          agile environments. Adept at developing innovative features, improving
+          existing systems, creating efficient internal tools, and debugging
+          issues within business applications. Approaches work optimistically to
+          think big while balancing practicality to consistently achieve goals.
+        </p>
+        <p className={styles.content}>
+          Additionally, despite growing up very much a math and science kid, has
+          learned to love and nurture creativity through writing, cartography,
+          and board game design. Other sources of joy are bouldering, bike
+          rides, art shows, and visiting friends who have landed in faraway
+          places. Finally, roller hockey, punk rock, and zines were big
+          influences in the past that provided a lot of learnings around team
+          work, expressiveness, and how the world could be a more equitable
+          place.
+        </p>
+        <p className={styles.content}>
+          Yes to racial justice, social justice, climate justice, and economic
+          justice.
+        </p>
       </div>
       <div className={styles.imgContainer}>
         <Image
