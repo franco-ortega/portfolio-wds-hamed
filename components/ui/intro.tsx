51baf8ff485de0a9232823782469f4b21028c73e
Franco Ortega <francopdx@gmail.com>
2024-08-28 18:59:40 -0700

    create ImageContainer

--- components/ui/intro.tsx
+++ components/ui/intro.tsx
@@ -1,5 +1,5 @@
 import Image, { StaticImageData } from 'next/image'
-import introImage from '@/public/images/intro/franco.jpg'
+import introImage from '/public/images/intro/franco.jpg'
 
 type ImageProps = {
   img: StaticImageData
