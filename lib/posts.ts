bf32bf61458a2ffa3ee16b10e46ed980187283c2
Franco Ortega <francopdx@gmail.com>
2024-09-03 17:27:11 -0700

    add types and error handling to getPostBySlug

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,14 +4,32 @@ import path from 'path'
 
 const rootDirectory = path.join(process.cwd(), 'content', 'posts')
 
-export async function getPostBySlug(slug: string) {
-  const filePath = path.join(rootDirectory, `${slug}.mdx`)
-  const fileContents = fs.readFileSync(filePath, 'utf8')
+export type PostMetadata = {
+  title?: string
+  summary?: string
+  image?: string
+  author?: string
+  publishedAt?: string
+  slug?: string
+}
 
-  const { data, content } = matter(fileContents)
+export type Post = {
+  metadata: PostMetadata
+  content: string
+}
 
-  return {
-    metaData: { ...data, slug },
-    content
+export async function getPostBySlug(slug: string): Promise<Post | null> {
+  try {
+    const filePath = path.join(rootDirectory, `${slug}.mdx`)
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+
+    const { data, content } = matter(fileContent)
+
+    return {
+      metadata: { ...data, slug },
+      content
+    }
+  } catch (error) {
+    return null
   }
 }
