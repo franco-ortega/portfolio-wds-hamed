132e570f5e61e8e96fafbe0fc873ac7eb7e35886
Franco Ortega <francopdx@gmail.com>
2024-09-16 17:03:23 -0700

    add temp list for Posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,6 +3,13 @@ export default async function PostsPage() {
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <h1 className='title mb-12'>Posts</h1>
+        <ul>
+          <li>Post 1</li>
+          <li>Post 2</li>
+          <li>Post 3</li>
+          <li>Post 4</li>
+          <li>Post 5</li>
+        </ul>
       </div>
     </section>
   )
