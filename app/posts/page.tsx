fb1aa7bccb23851526ba499af3c1cf51868321ed
Franco Ortega <francopdx@gmail.com>
2024-09-16 17:04:22 -0700

    make temp Post list more robust to better mimic actual Post list

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,11 +4,46 @@ export default async function PostsPage() {
       <div className='container max-w-3xl'>
         <h1 className='title mb-12'>Posts</h1>
         <ul>
-          <li>Post 1</li>
-          <li>Post 2</li>
-          <li>Post 3</li>
-          <li>Post 4</li>
-          <li>Post 5</li>
+          <li>
+            <h2>Post 1</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+              excepturi earum impedit recusandae labore tenetur vero enim alias
+              explicabo, dolor iste cumque cum tempora debitis.
+            </p>
+          </li>
+          <li>
+            <h2>Post 2</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+              excepturi earum impedit recusandae labore tenetur vero enim alias
+              explicabo, dolor iste cumque cum tempora debitis.
+            </p>
+          </li>
+          <li>
+            <h2>Post 3</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+              excepturi earum impedit recusandae labore tenetur vero enim alias
+              explicabo, dolor iste cumque cum tempora debitis.
+            </p>
+          </li>
+          <li>
+            <h2>Post 4</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+              excepturi earum impedit recusandae labore tenetur vero enim alias
+              explicabo, dolor iste cumque cum tempora debitis.
+            </p>
+          </li>
+          <li>
+            <h2>Post 5</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+              excepturi earum impedit recusandae labore tenetur vero enim alias
+              explicabo, dolor iste cumque cum tempora debitis.
+            </p>
+          </li>
         </ul>
       </div>
     </section>
